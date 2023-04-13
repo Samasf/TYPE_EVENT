1aae128f48d41a5a3b60e25c5c1160aca3396a95
felipe samas <ffelipesamas@gmail.com>
2023-04-12 21:11:20 -0300

    created Eventos

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,58 @@
-# from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib import auth, messages
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 
 # Create your views here.
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR,
+                                 'As senhas não coincidem!')
+
+            return redirect(reverse('cadastro'))
+
+        # todo: Validar força da senha
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.add_message(request, constants.ERROR,
+                                 'Usuario ja cadastrado!')
+            return redirect(reverse('cadastro'))
+
+        user = User.objects.create_user(
+            username=username, email=email, password=senha)
+
+        messages.add_message(request, constants.SUCCESS,
+                             'Usuario Salvo com Sucesso!')
+
+        return redirect(reverse('login'))
+
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = auth.authenticate(username=username,  password=senha)
+
+        if not user:
+            messages.add_message(request, constants.ERROR,
+                                 'Username ou senha invalidos!')
+            return redirect(reverse('login'))
+
+        auth.login(request, user)
+        return redirect('/evento/novo_evento/')
